8ad910eb7736f9c5fcae1a7a5d0e0f0114e4eb13
djrobson <djrobson@users.noreply.github.com>
2023-12-01 12:47:45 -0500

    solve day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,48 +1,77 @@
 aoc23_rust::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    /*let result : u32 = input
+    let total = input
         .lines()
         .map(|line| line.chars().filter(|c| c.is_numeric()).collect::<String>())
-        .map(|filtered_line| {
-            let digits = filtered_line.as_bytes();
-            let first = digits[0];
-            let last = digits[digits.len()-1];
-            let result: u32 = (u32::from(first) * 10) + u32::from(last);
-            result
+        .map(|line| {
+            let digits = line.as_bytes();
+            let first = digits[0] - 0x30;
+            let last = digits[digits.len() - 1] - 0x30;
+            u32::from(first * 10 + last)
         })
         .sum();
-        let lines = input.lines();
-    let all_nums: Vec<String> = lines
-        .map(|line| line.chars().filter(|c| c.is_numeric())
-        .collect::<String>()).collect();
-
-    let fl_nums:Vec<u32> = all_nums.iter().map(|line| {
-        let digits= line.as_bytes();
-        let first = digits[0] - 0x30;
-        let last = digits[digits.len()-1] - 0x30;
-        u32::from(first*10+last)
-    }).collect();
-
-    Some(fl_nums.iter().sum())
-    */
-    //Some(result)
-
-    let total = input.lines()
-        .map(|line| line.chars().filter(|c| c.is_numeric())
-        .collect::<String>())
-        .map(|line| {
-            let digits= line.as_bytes();
-            let first = digits[0] - 0x30;
-            let last = digits[digits.len()-1] - 0x30;
-            u32::from(first*10+last)
-        }).sum();
     Some(total)
 }
 
+fn is_ascii_numeric(input: u8) -> bool {
+    input >= b'1' && input <= b'9'
+}
 pub fn part_two(input: &str) -> Option<u32> {
+    let patterns = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
 
-    Some(281)
+    let lines: Vec<String> = input.lines().map(|line| line.to_string()).collect();
+
+    // convert 'one' 'two' 'three' to 1 2 3
+    let all_nums: Vec<Vec<u8>> = lines
+        .iter()
+        //.take(30)
+        .map(|line| {
+            let mut nums: Vec<u8> = Vec::new();
+            let mut offset = 0;
+            'location: while offset < line.len() {
+                if is_ascii_numeric(line.as_bytes()[offset]) {
+                    nums.push(line.as_bytes()[offset] - b'0');
+                    offset += 1;
+                    continue 'location;
+                }
+                for pat in patterns {
+                    if line[offset..].starts_with(pat.0) {
+                        nums.push(pat.1);
+                        // overlapping substrings are allowed in the inputs
+                        // offset += pat.0.len();
+                        offset += 1;
+                        continue 'location;
+                    }
+                }
+                offset += 1;
+            }
+            let num = {
+                let first = nums[0];
+                let last = nums[nums.len() - 1];
+                u32::from(first * 10 + last)
+            };
+            //println!("{} became {:?} with val {}", line, &nums, num);
+            nums
+        })
+        .collect();
+
+    let fl_nums = all_nums.iter().map(|line| {
+        let first = line[0];
+        let last = line[line.len() - 1];
+        u32::from(first * 10 + last)
+    });
+    Some(fl_nums.sum())
 }
 
 #[cfg(test)]
@@ -57,7 +86,12 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&aoc23_rust::template::read_file("examples", DAY));
+        let result = part_two(&aoc23_rust::template::read_second_example("examples", DAY));
+        assert_eq!(result, Some(281));
+    }
+    #[test]
+    fn test_part_two_real() {
+        let result = part_two(&aoc23_rust::template::read_file("inputs", DAY));
         assert_eq!(result, Some(281));
     }
 }
