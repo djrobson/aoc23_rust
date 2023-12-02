76c2f137b1a077244be62fcf877b37154d5b10d1
djrobson <drobson@gmail.com>
2023-12-02 08:52:13 -0500

    solve day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,23 +1,8 @@
 aoc23_rust::solution!(2);
 use std::collections::HashMap;
 
+
 fn parse_input(input: &str) -> Vec<HashMap<&str, u32>> {
-    input
-        .trim()
-        .split('\n')
-        .map(|line| {
-            let mut game = HashMap::new();
-            let parts: Vec<&str> = line.split(": ").collect();
-            let colors: Vec<&str> = parts[1].split("; ").collect();
-            for color in colors {
-                let counts: Vec<&str> = color.split(", ").collect();
-                game.insert(counts[1], counts[0].parse().unwrap());
-            }
-            game
-        })
-        .collect()
-}
-fn parse_input1(input: &str) -> Vec<HashMap<&str, u32>> {
     input
         .trim()
         .split('\n')
@@ -48,22 +33,19 @@ fn parse_input1(input: &str) -> Vec<HashMap<&str, u32>> {
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
-    let games = parse_input1(input);
+    let games = parse_input(input);
+    
     // look for games with only 12 red cubes, 13 green cubes, and 14 blue cubes
-
     let total = games
         .iter()
         .enumerate()
         .map(|(game_num, colors)| {
-            //dbg!("{}{:?}", game_num, colors);
             if colors.get("red").is_some_and(|cnt| cnt <= &12)
                 && colors.get("green").is_some_and(|cnt| cnt <= &13)
                 && colors.get("blue").is_some_and(|cnt| cnt <= &14)
             {
-                //dbg!("match");
                 game_num + 1 // enumerate counts from 0
             } else {
-                //dbg!("no match");
                 0
             }
         })
@@ -72,7 +54,18 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let games = parse_input(input);
+
+    let total = games
+        .iter()
+        .map(|colors| {
+            let red = colors.get("red").unwrap_or(&0);
+            let blue = colors.get("blue").unwrap_or(&0);
+            let green = colors.get("green").unwrap_or(&0);
+            red*blue*green // multiple the minum values together
+        })
+        .sum(); // return the sum of the values
+    Some(total)
 }
 
 #[cfg(test)]
@@ -88,6 +81,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&aoc23_rust::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
