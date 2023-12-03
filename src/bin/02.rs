e8f361e8b9f09db53c669c039ea960111f7457ea
djrobson <drobson@gmail.com>
2023-12-03 09:16:50 -0500

    solve day 3 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,7 +1,6 @@
 aoc23_rust::solution!(2);
 use std::collections::HashMap;
 
-
 fn parse_input(input: &str) -> Vec<HashMap<&str, u32>> {
     input
         .trim()
@@ -34,7 +33,7 @@ fn parse_input(input: &str) -> Vec<HashMap<&str, u32>> {
 
 pub fn part_one(input: &str) -> Option<usize> {
     let games = parse_input(input);
-    
+
     // look for games with only 12 red cubes, 13 green cubes, and 14 blue cubes
     let total = games
         .iter()
@@ -62,7 +61,7 @@ pub fn part_two(input: &str) -> Option<u32> {
             let red = colors.get("red").unwrap_or(&0);
             let blue = colors.get("blue").unwrap_or(&0);
             let green = colors.get("green").unwrap_or(&0);
-            red*blue*green // multiple the minum values together
+            red * blue * green // multiple the minum values together
         })
         .sum(); // return the sum of the values
     Some(total)
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,27 +1,16 @@
 aoc23_rust::solution!(3);
 
-#[derive(Debug,Clone,Copy)]
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Part {
     num: u32,
     start_row: usize,
     start_col: usize,
-    end_row: usize,
+    _end_row: usize,
     end_col: usize,
 }
-
-pub fn part_one(input: &str) -> Option<u32> {
-    // list of valid symbols
-    let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '-','/','='];
-
-    // read the input into a 2d vec
-    let input_grid: Vec<Vec<char>> = input
-        .trim()
-        .lines()
-        .map(|line| line.chars().collect())
-        .collect();
-    //dbg!(&input_grid);
-
-    // find the start/end location of numbers in the input
+fn get_parts_from_input(input_grid: &Vec<Vec<char>>) -> Vec<Part> {
     let mut row = 0;
     let mut parts: Vec<Part> = Vec::new();
     while row < input_grid.len() {
@@ -43,48 +32,147 @@ pub fn part_one(input: &str) -> Option<u32> {
                     num: next_num.parse::<u32>().unwrap(),
                     start_row: start_row,
                     start_col: start_col,
-                    end_row: end_row,
+                    _end_row: end_row,
                     end_col: end_col,
                 });
             } else {
-                col +=1;
+                col += 1;
             }
         }
         row += 1;
     }
+    parts
+}
+
+fn parse_input_grid(input: &str) -> Vec<Vec<char>> {
+    input
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect()
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    // list of valid symbols
+    let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '-', '/', '='];
+
+    // read the input into a 2d vec
+    let input_grid: Vec<Vec<char>> = parse_input_grid(input);
+    //dbg!(&input_grid);
+
+    // find the start/end location of numbers in the input
+    let parts = get_parts_from_input(&input_grid);
     //dbg!(&parts);
 
-    let adjascent = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)];
+    let adjascent = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
     // for each number, check if it has an adjascent symbol
-    let total = parts.iter().filter_map(|part| {
-        let row = part.start_row;
-        let mut col = part.start_col;
-        let mut found_symbol = false;
-        while col < part.end_col && found_symbol == false {
-            for &(dx, dy) in adjascent.iter() {
-                let new_row = row as i32 + dx;
-                let new_col = col as i32 + dy;
-                if new_row >= 0 && new_col >= 0 && new_row < input_grid.len() as i32 && new_col < input_grid[row].len() as i32 {
-                    if symbols.contains(&input_grid[new_row as usize][new_col as usize]) {
-                        found_symbol = true;
-                        break;
+    let total = parts
+        .iter()
+        .filter_map(|part| {
+            let row = part.start_row;
+            let mut col = part.start_col;
+            let mut found_symbol = false;
+            while col < part.end_col && found_symbol == false {
+                for &(dx, dy) in adjascent.iter() {
+                    let new_row = row as i32 + dx;
+                    let new_col = col as i32 + dy;
+                    if new_row >= 0
+                        && new_col >= 0
+                        && new_row < input_grid.len() as i32
+                        && new_col < input_grid[row].len() as i32
+                    {
+                        if symbols.contains(&input_grid[new_row as usize][new_col as usize]) {
+                            found_symbol = true;
+                            break;
+                        }
                     }
                 }
+                col += 1;
             }
-            col += 1;
-        }
-        if found_symbol {
-            Some(part.num)
-        } else {
-            None
-        }
-    }).sum();
+            if found_symbol {
+                Some(part.num)
+            } else {
+                None
+            }
+        })
+        .sum();
 
     Some(total)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // read the input into a 2d vec
+    let input_grid: Vec<Vec<char>> = parse_input_grid(input);
+
+    // find the start/end location of numbers in the input
+    let parts = get_parts_from_input(&input_grid);
+
+    let adjascent = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    let mut muls: HashMap<(usize, usize), Vec<Part>> = HashMap::new();
+    for (row_index, row) in input_grid.iter().enumerate() {
+        for (col_index, &cell) in row.iter().enumerate() {
+            if cell == '*' {
+                muls.insert((row_index, col_index), Vec::new());
+            }
+        }
+    }
+    // for each number, check if it has an adjascent symbol
+    let _valid_parts = parts
+        .iter()
+        .map(|part| {
+            let row = part.start_row;
+            let mut col = part.start_col;
+            while col < part.end_col {
+                for &(dx, dy) in adjascent.iter() {
+                    let new_row = row.saturating_add_signed(dx);
+                    let new_col = col.saturating_add_signed(dy);
+                    if new_row < input_grid.len() && new_col < input_grid[row].len() {
+                        if &input_grid[new_row][new_col] == &'*' {
+                            let mul = muls.get_mut(&(new_row, new_col)).expect("found new *");
+                            if !mul.contains(&part) {
+                                mul.push(part.clone());
+                                break;
+                            }
+                        }
+                    }
+                }
+                col += 1;
+            }
+        })
+        .count();
+
+    //dbg!(&muls);
+    let total = muls
+        .iter()
+        .map(|((_row, _col), parts)| {
+            if parts.len() == 2 {
+                parts[0].num * parts[1].num
+            } else {
+                0
+            }
+        })
+        .sum();
+    Some(total)
 }
 
 #[cfg(test)]
@@ -100,6 +188,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&aoc23_rust::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
