1eb71ed25783076d8e6687692fee3de4bc1cb0c1
djrobson <drobson@gmail.com>
2023-12-17 00:11:06 -0500

    day 17 start

---
+++ src/bin/17.rs
@@ -0,0 +1,26 @@
+aoc23_rust::solution!(17);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&aoc23_rust::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&aoc23_rust::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
