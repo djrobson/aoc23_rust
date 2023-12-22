fc70846c3fb2c553b567bfe5244310eefa06363b
djrobson <djrobson@users.noreply.github.com>
2023-12-22 13:57:03 -0500

    day 22 input parsing

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,6 +1,37 @@
 aoc23_rust::solution!(22);
 
+pub fn parse_input(input: &str) -> Vec<((i32, i32, i32), (i32, i32, i32))> {
+    let mut result = Vec::new();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split('~').collect();
+        if parts.len() != 2 {
+            panic!("Invalid input ~");
+        }
+
+        let parse_tuple = |s: &str| -> Option<(i32, i32, i32)> {
+            let nums: Vec<i32> = s.split(',').filter_map(|x| x.parse().ok()).collect();
+            if nums.len() == 3 {
+                Some((nums[0], nums[1], nums[2]))
+            } else {
+                None
+            }
+        };
+
+        if let (Some(t1), Some(t2)) = (parse_tuple(parts[0]), parse_tuple(parts[1])) {
+            result.push((t1, t2));
+        } else {
+            panic!("Invalid input i32s");
+        }
+    }
+
+    result
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+
+    let blocks = parse_input(input);
+
     None
 }
 
