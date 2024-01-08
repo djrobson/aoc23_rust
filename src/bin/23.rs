3245cc6afa78b9e87baf9e3a3bdf4fbfda52d798
djrobson <drobson@gmail.com>
2024-01-07 21:15:26 -0500

    day 23 part 1

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,46 +1,35 @@
 aoc23_rust::solution!(23);
 use glam::IVec2;
+use std::collections::{HashMap, VecDeque};
 
-use std::collections::{HashMap, HashSet, VecDeque};
-
-fn parse_input(input: &str) -> (Vec<Vec<char>>, IVec2) {
-    let lines: Vec<&str> = input.lines().collect();
-    let mut array_2d = Vec::new();
-
-    let mut position_s = IVec2::splat(0);
-
-    for (y, line) in lines.iter().enumerate() {
-        let elements: Vec<char> = line.chars().collect();
-        if let Some(x) = elements.iter().position(|&c| c == 'S') {
-            position_s = IVec2::new(x as i32, y as i32);
-        }
-        array_2d.push(elements);
-    }
-
-    (array_2d, position_s)
+fn parse_input(input: &str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .map(|line| line.trim().bytes().collect())
+        .collect()
 }
 
-fn get_vertices(grid: &Vec<Vec<char>>) -> Vec<IVec2> {
+fn get_vertices(grid: &Vec<Vec<u8>>) -> Vec<IVec2> {
     let mut vertices = Vec::new();
 
     for (y, line) in grid.iter().enumerate() {
         for (x, &c) in line.iter().enumerate() {
-            if c == '#' {
+            if c == b'#' {
                 continue;
             }
 
             // get the characters up, left, down, right from xy, if at least 2 of them are in "<>^v" then xy is a vertex
             let mut count = 0;
-            if x > 0 && "<>^v".contains(grid[y][x - 1]) {
+            if x > 0 && "<>^v".contains(grid[y][x - 1] as char) {
                 count += 1;
             }
-            if x < grid[0].len() - 1 && "<>^v".contains(grid[y][x + 1]) {
+            if x < grid[0].len() - 1 && "<>^v".contains(grid[y][x + 1] as char) {
                 count += 1;
             }
-            if y > 0 && "<>^v".contains(grid[y - 1][x]) {
+            if y > 0 && "<>^v".contains(grid[y - 1][x] as char) {
                 count += 1;
             }
-            if y < grid.len() - 1 && "<>^v".contains(grid[y + 1][x]) {
+            if y < grid.len() - 1 && "<>^v".contains(grid[y + 1][x] as char) {
                 count += 1;
             }
 
@@ -57,121 +46,204 @@ fn get_vertices(grid: &Vec<Vec<char>>) -> Vec<IVec2> {
     vertices
 }
 
-fn get_next_positions(grid: &Vec<Vec<char>>, position: IVec2) -> Vec<IVec2> {
-    let mut next_positions = Vec::new();
+fn check_grid_for_forks(grid: &Vec<Vec<u8>>) -> bool {
+    let mut found_fork = false;
+    for (y, line) in grid.iter().enumerate() {
+        for (x, &c) in line.iter().enumerate() {
+            if c == b'#' {
+                continue;
+            }
 
-    let x: usize = position.x as usize;
-    let y: usize = position.y as usize;
+            // get the characters up, left, down, right from xy, if at least 2 of them are in "<>^v" then xy is a vertex
+            let mut count = 0;
+            if x > 0 && grid[y][x - 1] == b'.' {
+                count += 1;
+            }
+            if x < grid[0].len() - 1 && grid[y][x + 1] == b'.' {
+                count += 1;
+            }
+            if y > 0 && grid[y - 1][x] == b'.' {
+                count += 1;
+            }
+            if y < grid.len() - 1 && grid[y + 1][x] == b'.' {
+                count += 1;
+            }
 
-    // if current position in < > ^ v then only go left right up or down, respectively
-    match grid[y][x] {
-        '<' => {
-            if x > 0 && grid[y][x - 1] != '#' {
-                next_positions.push(position + IVec2::NEG_X);
-            }
-        }
-        '>' => {
-            if x < grid[0].len() - 1 && grid[y][x + 1] != '#' {
-                next_positions.push(position + IVec2::X);
-            }
-        }
-        '^' => {
-            if y > 0 && grid[y - 1][x] != '#' {
-                next_positions.push(position + IVec2::NEG_Y);
-            }
-        }
-        'v' => {
-            if y < grid.len() - 1 && grid[y + 1][x] != '#' {
-                next_positions.push(position + IVec2::Y);
-            }
-        }
-        _ => {
-            if x > 0 && grid[y][x - 1] != '#' {
-                next_positions.push(position + IVec2::NEG_X);
-            }
-            if x < grid[0].len() - 1 && grid[y][x + 1] != '#' {
-                next_positions.push(position + IVec2::X);
-            }
-            if y > 0 && grid[y - 1][x] != '#' {
-                next_positions.push(position + IVec2::NEG_Y);
-            }
-            if y < grid.len() - 1 && grid[y + 1][x] != '#' {
-                next_positions.push(position + IVec2::Y);
+            if count >= 3 {
+                println!("fork at {},{}", x, y);
+                found_fork = true;
             }
         }
     }
+    found_fork
+}
 
-    // else go to any direction that's not a #
+fn get_edges_from_node(grid: &Vec<Vec<u8>>, node: IVec2) -> Vec<IVec2> {
+    let mut egress = Vec::new();
 
-    next_positions
+    let x: usize = node.x as usize;
+    let y: usize = node.y as usize;
+
+    // for each direction check if there is a path leaving the current node
+    // if there is a path, then follow it until you reach a vertex
+    if (node + IVec2::X).x < grid[0].len() as i32 && grid[y][x + 1] == b'>' {
+        egress.push(node + IVec2::X);
+    }
+    if (node - IVec2::X).x >= 0 && grid[y][x - 1] == b'<' {
+        egress.push(node - IVec2::X);
+    }
+    if (node + IVec2::Y).y < grid.len() as i32 && grid[y + 1][x] == b'v' {
+        egress.push(node + IVec2::Y);
+    }
+    if (node - IVec2::Y).y >= 0 && grid[y - 1][x] == b'^' {
+        egress.push(node - IVec2::Y);
+    }
+
+    egress
+}
+
+fn follow_path_until_vertex(grid: &[Vec<u8>], start: &IVec2, vertices: &[IVec2]) -> (IVec2, usize) {
+    let mut path_length = 1;
+    let mut current_node = *start;
+    let mut previous_node = match grid[start.y as usize][start.x as usize] {
+        b'>' => current_node - IVec2::X,
+        b'<' => current_node + IVec2::X,
+        b'v' => current_node - IVec2::Y,
+        b'^' => current_node + IVec2::Y,
+        _ => panic!("invalid start node"),
+    };
+
+    assert!(grid[current_node.y as usize][current_node.x as usize] != b'#');
+
+    // continue following the path of b'.' until you reach a vertex, don't back track
+    loop {
+        let available_edges: Vec<IVec2> = [IVec2::X, IVec2::Y, -IVec2::X, -IVec2::Y]
+            .iter()
+            .flat_map(|edge| {
+                let possible_next = current_node + *edge;
+                if grid[possible_next.y as usize][possible_next.x as usize] != b'#'
+                    && possible_next != previous_node
+                {
+                    Some(possible_next)
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<IVec2>>();
+
+        if available_edges.len() != 1 {
+            panic!("bad path at {},{}", current_node.x, current_node.y);
+        }
+
+        path_length += 1;
+        if vertices.contains(&available_edges[0]) {
+            current_node = available_edges[0];
+            break;
+        }
+
+        // update cursors and keep walking
+        previous_node = current_node;
+        current_node = available_edges[0];
+    }
+
+    (current_node, path_length)
 }
 
 // print dominator graph in DOT format
-fn print_dominator_graph(dominator_graph: &HashMap<IVec2, Vec<(IVec2, usize)>>) {
+#[allow(dead_code)]
+fn print_graph(graph: &HashMap<IVec2, HashMap<IVec2, usize>>) {
     println!("digraph G {{");
-    for (vertex, dominated) in dominator_graph {
-        for (dominated_vertex, _) in dominated {
-            println!("  \"{}\" -> \"{}\"", vertex, dominated_vertex);
+    for vertex in graph.keys() {
+        for other_vertex in graph.get(vertex).unwrap().keys() {
+            println!(
+                "  \"{}\" -> \"{}\" [weight={}]",
+                vertex,
+                other_vertex,
+                graph.get(vertex).unwrap().get(other_vertex).unwrap()
+            );
         }
     }
     println!("}}");
 }
 
+fn get_graph(vertices: &[IVec2], grid: &Vec<Vec<u8>>) -> HashMap<IVec2, HashMap<IVec2, usize>> {
+    let mut graph: HashMap<IVec2, HashMap<IVec2, usize>> = HashMap::new();
+
+    let mut nodes: VecDeque<IVec2> = VecDeque::new();
+    let start = IVec2::new(1, 0);
+
+    // walk the vertices starting from the start node
+    nodes.push_back(start);
+    while let Some(vertex) = nodes.pop_front() {
+        if graph.contains_key(&vertex) {
+            continue; // we already processed this one
+        }
+
+        // find all adjacent vertices with their cost
+        let egress = get_edges_from_node(grid, vertex);
+        let edges = egress
+            .iter()
+            .map(|&edge| follow_path_until_vertex(grid, &edge, vertices))
+            .collect::<Vec<(IVec2, usize)>>();
+
+        let mut adj_nodes = HashMap::new();
+        for edge in edges {
+            adj_nodes.insert(edge.0, edge.1);
+
+            // add the adjacent vertices to the queue
+            nodes.push_back(edge.0);
+        }
+        graph.insert(vertex, adj_nodes);
+    }
+
+    graph
+}
+
+fn find_worst_cost(
+    graph: &HashMap<IVec2, HashMap<IVec2, usize>>,
+    start: &IVec2,
+    end: &IVec2,
+) -> usize {
+    let mut worst_found: HashMap<IVec2, usize> = HashMap::new();
+    //worst_found.insert(*start, 0);
+    let mut process: VecDeque<(IVec2, usize)> = VecDeque::new();
+    process.push_back((*start, 0));
+    while let Some((vertex, cost)) = process.pop_front() {
+        if worst_found.contains_key(&vertex) {
+            // update vertex if we found something worse
+            if &cost > worst_found.get(&vertex).unwrap() {
+                worst_found.insert(vertex, cost);
+            } else {
+                continue;
+            }
+        } else {
+            worst_found.insert(vertex, cost);
+        }
+        for (adj_vertex, adj_cost) in graph.get(&vertex).unwrap() {
+            process.push_back((*adj_vertex, cost + adj_cost));
+        }
+    }
+    *worst_found.get(end).unwrap()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let (grid, start) = parse_input(input);
-    let end = IVec2::new((grid[0].len() - 1) as i32, (grid.len() - 2) as i32); // example and input ending is just left of the bottom right corner
+    let grid = parse_input(input);
+    assert!(!check_grid_for_forks(&grid));
+
+    // example and input ending is just left of the bottom right corner
+    let start = IVec2::new(1, 0);
+    let end = IVec2::new((grid[0].len() - 2) as i32, (grid.len() - 1) as i32);
 
     let mut vertices = get_vertices(&grid);
     vertices.push(start);
     vertices.push(end);
 
-    // find the topological dominator graph for all vertices on grid
-    // for each pair of vertices, find any path between them that doesn't go through any other vertex
-    // if there is a path from the first vertex to the second, then the first vertex is a dominator of the second
-    // append the second vertex to the dominated list of the first vertex
-    let mut dominator_graph: HashMap<IVec2, Vec<(IVec2, usize)>> = HashMap::new();
-    for vertex1 in &vertices {
-        for vertex2 in &vertices {
-            if (vertex1.x, vertex1.y) == (vertex2.x, vertex2.y) {
-                continue;
-            }
+    // find theweighted graph for all vertices on grid
+    let graph = get_graph(&vertices, &grid);
 
-            let mut path = HashSet::new();
-            let mut path_options = VecDeque::new();
-            path_options.push_back(*vertex1);
+    //print_graph(&graph);
 
-            while let Some(position) = path_options.pop_front() {
-                if path.contains(&position) {
-                    continue;
-                }
-
-                path.insert(position);
-
-                if position == *vertex2 {
-                    // check is there's already a path between the two vertices and keep the longer one???
-                    dominator_graph
-                        .entry(*vertex1)
-                        .or_default()
-                        .push((*vertex2, path.len()));
-                    break;
-                }
-
-                for next_position in get_next_positions(&grid, position) {
-                    path_options.push_back(next_position);
-                }
-            }
-        }
-    }
-
-    print_dominator_graph(&dominator_graph);
-
-    // for each vertes in the dominator graph do a depth first search to find the longest path
-    // if you reach a vertex for the first time then record the path length and vertices traversed
-    // if you reach a vertex for the second time then check if the path length is longer than the previous one
-    // if the vertex is the end vertex, then the cost is the length of the path from the start vertex
-    // if the vertex is not the end vertex, then the cost is the length of the path from the start vertex plus the highest cost of the dominated vertices
-
-    None
+    Some(find_worst_cost(&graph, &start, &end) as u32)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -182,11 +254,17 @@ pub fn part_two(_input: &str) -> Option<u32> {
 mod tests {
     use super::*;
 
-    #[ignore]
     #[test]
     fn test_part_one() {
         let result = part_one(&aoc23_rust::template::read_file("examples", DAY));
         assert_eq!(result, Some(94));
+    }
+
+    #[ignore]
+    #[test]
+    fn test_part_one_real() {
+        let result = part_one(&aoc23_rust::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(2414));
     }
 
     #[ignore]
